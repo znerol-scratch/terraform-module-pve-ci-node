c5a12328745f1b3f1e1cd067d5a503713b1aea38
znerol <lo+github@znerol.ch>
2025-01-06 22:49:53 +0100

    Do not try to start VM

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "node_url" {
-  value       = "https://${local.address}:8006/"
-  description = "Web URL of the PVE manager."
-}
+# output "node_url" {
+#   value       = "https://${local.address}:8006/"
+#   description = "Web URL of the PVE manager."
+# }
 
 output "password" {
   value       = local.use_generated_password ? random_password.root[0].result : ""
