8624c8e3676d8c48874e5f4c165511b5fc484e00
znerol <lo+github@znerol.ch>
2025-01-07 17:37:52 +0100

    Enable libvirt verbose logging

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@
 
 locals {
   nodename               = var.nodename == "" ? var.name_prefix : var.nodename
-  # address                = libvirt_domain.node.network_interface[0].addresses[0]
+  address                = libvirt_domain.node.network_interface[0].addresses[0]
   use_generated_keypair  = var.ssh_private_key == "" || var.ssh_public_key == ""
   ssh_public_key         = local.use_generated_keypair ? tls_private_key.keypair[0].public_key_openssh : var.ssh_public_key
   ssh_private_key        = local.use_generated_keypair ? tls_private_key.keypair[0].private_key_openssh : var.ssh_private_key
@@ -52,48 +52,48 @@ resource "libvirt_volume" "root" {
   size           = 21474836480
 }
 
-# resource "libvirt_domain" "node" {
-#   name       = var.name_prefix
-#   machine    = "q35"
-#   cloudinit  = libvirt_cloudinit_disk.ci.id
-#   memory     = 2048
-#   vcpu       = 2
-#   qemu_agent = true
-#   xml {
-#     xslt = file("${path.module}/libvirt-domain.xsl")
-#   }
-#   cpu {
-#     mode = "host-passthrough"
-#   }
-#   disk {
-#     volume_id = libvirt_volume.root.id
-#     scsi      = true
-#   }
-#   network_interface {
-#     network_name   = var.libvirt_network_name
-#     hostname       = local.nodename
-#     wait_for_lease = true
-#   }
-#   console {
-#     type        = "pty"
-#     target_port = "0"
-#     target_type = "serial"
-#   }
-#   provisioner "remote-exec" {
-#     inline = [
-#       "cloud-init status --wait",
-#       templatefile("${path.module}/provision/setup-hostname.sh.tftpl", {
-#         hostname = local.nodename
-#         ipv4     = self.network_interface[0].addresses[0]
-#       }),
-#       file("${path.module}/provision/install-pve.sh"),
-#       file("${path.module}/provision/setup-interfaces-sdn.sh")
-#     ]
-#     connection {
-#       type        = "ssh"
-#       user        = "debian"
-#       private_key = local.ssh_private_key
-#       host        = self.network_interface[0].addresses[0]
-#     }
-#   }
-# }
+resource "libvirt_domain" "node" {
+  name       = var.name_prefix
+  machine    = "q35"
+  cloudinit  = libvirt_cloudinit_disk.ci.id
+  memory     = 2048
+  vcpu       = 2
+  qemu_agent = true
+  xml {
+    xslt = file("${path.module}/libvirt-domain.xsl")
+  }
+  cpu {
+    mode = "host-passthrough"
+  }
+  disk {
+    volume_id = libvirt_volume.root.id
+    scsi      = true
+  }
+  network_interface {
+    network_name   = var.libvirt_network_name
+    hostname       = local.nodename
+    wait_for_lease = true
+  }
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "cloud-init status --wait",
+      templatefile("${path.module}/provision/setup-hostname.sh.tftpl", {
+        hostname = local.nodename
+        ipv4     = self.network_interface[0].addresses[0]
+      }),
+      file("${path.module}/provision/install-pve.sh"),
+      file("${path.module}/provision/setup-interfaces-sdn.sh")
+    ]
+    connection {
+      type        = "ssh"
+      user        = "debian"
+      private_key = local.ssh_private_key
+      host        = self.network_interface[0].addresses[0]
+    }
+  }
+}
